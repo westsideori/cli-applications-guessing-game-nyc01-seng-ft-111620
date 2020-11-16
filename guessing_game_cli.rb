f3b820229b86e4dfe6f502e0f33260fcab94f76d
Ori Markowitz <ori.markowitz@gmail.com>
2020-11-16 00:13:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,4 +4,9 @@ def run_guessing_game
   secret_number.to_s
   puts "Guess a number between 1 and 6"
   guess = gets.chomp
-  
+  if guess == secret_number
+    puts "You guessed the correct number!"
+  elsif guess == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{secret_number}"
